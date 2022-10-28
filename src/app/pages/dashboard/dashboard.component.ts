9677a91c2cc7d0e2effb87b3f835d3c2c8b05ac8
light2000c <clintononitsha@gmail.com>
2022-10-28 16:58:53 +0100

    changed menu structure

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { promises } from 'dns';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  header = new Headers({
+    'Authorization': 'Bearer d24m6z0v5f06e0ogq97vc2k97710222022m08981pt6cr64v787l7tmav8m40936',
+    'Content-Type' : 'application/json',
+  });
+  constructor(
+    private http: HttpClient,
+  ) { }
 
   ngOnInit(): void {
+    this.check();
   }
+
+  check(){
+   const promise = new Promise((resolve, reject) => {
+   this.http.post("https://www.nellobytesystems.com/enterprise/APIGetWalletBalanceV1.asp",{headers: this.header}).subscribe((res)=>{
+      resolve(res);
+      console.log(res);
+    }, (error) => {
+      console.log(error);
+      reject(error);
+    });
+   });
+
+   return promise;
+  }
+
 
 }
--- src/app/services/http/http.service.ts
+++ src/app/services/http/http.service.ts
@@ -10,15 +10,7 @@ token = '8t01gc14r1nd8r45s9t13rfj8228120225qmp03dhu9q3bj0g4h90584wo121327'
 header = new HttpHeaders({
   'Authorization': `Bearer${this.token}`,
   'content-type': 'application/json',
-  'Accept': 'application/json',
-  'Cookie': 'ASPSESSIONIDQWDDBTCB=NGLLJEABCDEIAMDNCMIMGNBL',
-  'Postman-Token': '<calculated when request is sent>',
-  'Content-length': '0',
-  'Host': '<calculated when request is sent>',
-  'User-Agent': 'PostmanRuntime/7.29.2',
-  // 'Accept': '*/*',
-  'Accept-Encoding': 'gzip, deflate, br',
-  'Connection': 'keep-alive',
+
 });
 
   constructor(
